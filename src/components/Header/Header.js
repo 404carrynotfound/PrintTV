c0354ea5ed11fc5e393d8cf7cf0f51c6fbcf6b89
Kristiyan <37977687+404carrynotfound@users.noreply.github.com>
2021-12-26 15:46:33 +0200

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,14 +11,12 @@ export default function Header() {
         <AppBar position="static" color="default" elevation={0} sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }} >
             <Toolbar sx={{ flexWrap: 'wrap' }}>
                 <Typography variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }} />
-                <nav>
-                    <Link variant="button" color="text.primary" href="#" sx={{ my: 1, mx: 1.5 }} >
-                        Features
-                    </Link>
-                    <Link variant="button" color="text.primary" href="#" sx={{ my: 1, mx: 1.5 }} >
-                        Support
-                    </Link>
-                </nav>
+                <Link variant="button" color="text.primary" href="#" sx={{ my: 1, mx: 1.5 }} >
+                    Features
+                </Link>
+                <Link variant="button" color="text.primary" href="#" sx={{ my: 1, mx: 1.5 }} >
+                    Support
+                </Link>
                 <Button to="/login" variant="outlined" sx={{ my: 1, mx: 1.5 }} LinkComponent={RouterLink}>
                     Sign in
                 </Button>
