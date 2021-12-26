f6bb91765c7aa5e1f20971c9767d27e3137d5429
Kristiyan <37977687+404carrynotfound@users.noreply.github.com>
2021-12-26 23:08:22 +0200

    Add admin navigation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,10 +8,9 @@ import { Link as RouterLink } from 'react-router-dom';
 
 import { useAuthContext } from '../../contexts/AuthContext';
 
-
 export default function Header() {
     const { user } = useAuthContext();
-    
+
     const guestNav = (
         <>
             <Typography variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }} />
@@ -27,12 +26,24 @@ export default function Header() {
         </>
     );
 
-    const userNav = (
+    const adminNav = (
         <>
             <Typography variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }} />
             <Link variant="button" color="text.primary" href="#" sx={{ my: 1, mx: 1.5 }} >
                 Features
             </Link>
+            <Link variant="button" color="text.primary" to="/admin" sx={{ my: 1, mx: 1.5 }} component={RouterLink} >
+                Admin Panel
+            </Link>
+            <Button to="/logout" variant="outlined" sx={{ my: 1, mx: 1.5 }} LinkComponent={RouterLink}>
+                Logout
+            </Button>
+        </>
+    );
+
+    const userNav = (
+        <>
+            <Typography variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }} />
             <Link variant="button" color="text.primary" href="#" sx={{ my: 1, mx: 1.5 }} >
                 Support
             </Link>
@@ -40,12 +51,14 @@ export default function Header() {
                 Logout
             </Button>
         </>
-    )
+    );
 
     return (
         <AppBar position="static" color="default" elevation={0} sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }} >
             <Toolbar sx={{ flexWrap: 'wrap' }}>
-                {user.accessToken ? userNav : guestNav}
+                {!user.email && guestNav}
+                {user.email && user.isAdmin && adminNav}
+                {user.email && !user.isAdmin && userNav}
             </Toolbar>
         </AppBar>
     )
