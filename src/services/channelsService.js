27781eb650b1cbfa86714c84451c874e853402b9
Kristiyan <37977687+404carrynotfound@users.noreply.github.com>
2021-12-27 16:17:27 +0200

    Update channelsService.js

--- src/services/channelsService.js
+++ src/services/channelsService.js
@@ -4,20 +4,7 @@ const baseUrl = "https://print-tv-api.herokuapp.com/data";
 
 export const getAll = () => request.get(`${baseUrl}/playlist`);
 
-export const create = async (playlistData, token) => {
-    let response = await fetch(`${baseUrl}/playslist`, {
-        method: 'POST',
-        headers: {
-            'content-type': 'application/json',
-            'X-Authorization': token,
-        },
-        body: JSON.stringify({ ...playlistData })
-    });
-
-    let result = await response.json();
-
-    return result;
-};
+export const create = (channelData) => request.post(`${baseUrl}/playlist`, channelData);
 
 export const update = (channelId, channelData) => request.put(`${baseUrl}/playlist/${channelId}`, channelData);
 
